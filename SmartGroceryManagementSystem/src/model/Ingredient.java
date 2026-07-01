c364ce5d2ece9f5ae293feea3ec49b5caa769b76
nathali-sam <as20240628@sci.sjp.ac.lk>
2026-07-01 11:37:11 +0530

    Implemented Ingredient Class.

--- SmartGroceryManagementSystem/src/model/Ingredient.java
+++ SmartGroceryManagementSystem/src/model/Ingredient.java
@@ -9,5 +9,79 @@ package model;
  * @author perer
  */
 public class Ingredient {
-    
+    private int ingredientId;
+    private int productId;
+    private String name;
+    private String category;
+    private String unit;
+
+    public Ingredient() {}
+
+    public Ingredient(int ingredientId, int productId, String name, String category, String unit) {
+
+        this.ingredientId = ingredientId;
+        this.productId = productId;
+        this.name = name;
+        this.category = category;
+        this.unit = unit;
+    }
+
+    public void addIngredient() {
+        System.out.println("Ingredient added.");
+    }
+
+    public void updateIngredient() {
+        System.out.println("Ingredient updated.");
+    }
+
+    public void getAllIngredients() {
+        System.out.println("Displaying all ingredients.");
+    }
+
+    public int getIngredientId() {
+        return ingredientId;
+    }
+
+    public void setIngredientId(int ingredientId) {
+        this.ingredientId = ingredientId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(name != null && !name.isEmpty()){
+            this.name = name;
+        }
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + unit + ")";
+    }
 }
