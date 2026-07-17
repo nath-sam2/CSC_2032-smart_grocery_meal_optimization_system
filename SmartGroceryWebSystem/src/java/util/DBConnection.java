ce86189cb0bba9f7a43cd9a5fc0ccfb1bb1590dc
UshaniKavisha <as20240494@sci.sjp.ac.lk>
2026-07-17 23:55:29 +0530

    Added MySQL Connector/J 9.7.0 library for database connectivity and cart_item table added

--- SmartGroceryWebSystem/src/java/util/DBConnection.java
+++ SmartGroceryWebSystem/src/java/util/DBConnection.java
@@ -15,7 +15,7 @@ import java.sql.SQLException;
 
 public class DBConnection {
     // Database URL
-    private static final String URL = "jdbc:mysql://localhost:3306/smart_grocery_system_fina";
+    private static final String URL = "jdbc:mysql://localhost:3306/smart_grocery_system_final";
 
     // Database username
     private static final String USER = "root";
